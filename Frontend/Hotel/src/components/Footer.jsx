4c32981cd2b738e235c68e1747c3c7af1c212ac3
ayaksoy <yasir.aksoy@outlook.com>
2024-08-09 21:06:16 +0300

    Footer component

---
+++ Frontend/Hotel/src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<div id="footer">
+			<div className="container">
+				<div className="row">
+					<div className="col-12">
+						<div className="social">
+							<a href="">
+								<li className="fa fa-instagram"></li>
+							</a>
+							<a href="">
+								<li className="fa fa-twitter"></li>
+							</a>
+							<a href="">
+								<li className="fa fa-facebook-f"></li>
+							</a>
+						</div>
+					</div>
+					<div className="col-12">
+						<p>
+							Copyright &#169; 2045{" "}
+							<a href="https://www.github/ayaksoy" blank>
+								aaksoy
+							</a>{" "}
+							All Rights Reserved.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
