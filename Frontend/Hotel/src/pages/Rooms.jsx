4047cdcbe4814b8d7d25019db4d666a755971623
ayaksoy <yasir.aksoy@outlook.com>
2024-08-09 21:08:20 +0300

    Rooms page

---
+++ Frontend/Hotel/src/pages/Rooms.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchSection from "../components/SearchSection";
+import OurRooms from "../components/OurRooms";
+import Modal from "../components/Modal";
+
+export default function Rooms() {
+	return (
+		<>
+			<SearchSection />
+			<OurRooms />
+			<Modal />
+		</>
+	);
+}
